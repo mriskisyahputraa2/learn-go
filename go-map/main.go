3460c569d786ee59009a28cb7801103a634ce08e
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-01-30 13:33:19 +0700

    update learn map

--- go-map/main.go
+++ go-map/main.go
@@ -20,7 +20,9 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
@@ -72,9 +74,60 @@ func main() {
 	fmt.Println(computer)
 	fmt.Println("\n")
 
-	// OUTPUT MENDAPATKAN PANJANG MAP DENGAN FUNGSI LEN()
+	// OUTPUT (MENDAPATKAN PANJANG MAP DENGAN FUNGSI LEN())
 	fmt.Println("Panjang Map dari person adalah =", len(person))
 	fmt.Println("Panjang Map dari Detailperson adalah =", len(detailPerson))
+	fmt.Println("\n")
+
+	// MEMBUAT MAP KOSONG
+	var mapKosong1 = make(map[string]string)
+	var mapKosong2 map[string]string
+
+	fmt.Println(mapKosong1 == nil) // false, jika menggunakan make default nya false
+	fmt.Println(mapKosong2 == nil) // true, jika menggunakan map default nya true
+	fmt.Println("\n")
+
+	// OUTPUT (MENAMBAH DAN MENGUBAH ELEMEN MAP)
+	fmt.Println("data original person sebelum di tambahkan =", person)
+	fmt.Println("\n")
+
+	person["job"] = "kerja tempat orang"
+	person["salary"] = "gaji cuma 20 ribu"
+	fmt.Println(person["job"])
+	fmt.Println(person["salary"])
+	fmt.Println("data person sesudah di tambahkan yang baru =", person)
+	fmt.Println("\n")
+
+	// (OUPUT MENGHAPUS ELEMEN MAP)
+	fmt.Println("data original person sebelum di hapus =", person)
+	fmt.Println("\n")
+
+	delete(person, "salary") // menghapus key salary dari data person
+
+	fmt.Println("data original person sesudah di hapus =", person)
+	fmt.Println("sekarang panjang nya =", len(person))
+	fmt.Println("\n")
+
+	// MAP BERSIFAT PASS BY REFERENCE ARTINYA SETIAP VALUE UTAMA DIRUBAH MAKA AKAN BERDAMPAK PADA VALUE UTAMA
+	var mahasiswa = map[string]string{
+		"name":    "Muhammad Rizki Syahputra",
+		"jurusan": "Teknologi Informasi dan Komputer",
+		"prodi":   "Teknologi Rekayasa Komputer Jaringan",
+		"kelas":   "trkj 1.c",
+	}
+	fmt.Println("data original sebelum pass by reference =", mahasiswa)
+	fmt.Println("Nama =", mahasiswa["name"])
+	fmt.Println("Jurusan =", mahasiswa["jurusan"])
+	fmt.Println("Prodi =", mahasiswa["prodi"])
+	fmt.Println("Kelas =", mahasiswa["kelas"])
+	fmt.Println("\n")
+
+	mahasiswa["kelas"] = "trkj 1.a" // value dari kelas sudah diubah menjadi tkrj 1.a
+	fmt.Println("data original sesudah pass by reference =", mahasiswa)
+	fmt.Println("Nama =", mahasiswa["name"])
+	fmt.Println("Jurusan =", mahasiswa["jurusan"])
+	fmt.Println("Prodi =", mahasiswa["prodi"])
+	fmt.Println("Kelas =", mahasiswa["kelas"])
 	fmt.Println("\n")
 
 }

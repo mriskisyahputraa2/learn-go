543ced6f82163a68c15c3464b63fac6d3ff6a929
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-02-02 21:58:05 +0700

    learn update variadic function

---
+++ go-variadicFunction/main.go
@@ -0,0 +1,62 @@
+/* CATATAN
+
+# VARIADIC FUNCTION
+	- VARIADIC FUNCTION ADALAH SEBUAH FUNCTION YANG DAPAT MENERIMA SEJUMLAH VARIABEL
+	SEBAGAI ARGUMEN, ARGUMEN INI DISEBUT DENGAN "varargs"
+	- UNTUK MENDEKLARASI VARAIDIC FUNCTION, TIPE DATA PADA PARAMETER TERAKHIR DIDAHULUI
+	DENGAN ...(titik tiga)
+	- JENIS FUNGSI INI BERGUNA KETIKA KITA TIDAK MENGETAHUI JUMLAH ARGUMEN YANG KITA
+	BERIKAN KE FUNGSI TERSEBUT
+	- SALAH SATU CONTOH VARIADIC FUNCTION YAITU FUNGSI "Println", BAWAAN DARI PAKET "fmt"
+	- ... (titik tiga) BERFUNGSI UNTUK MEMBUAT BANYAK NILAI/VALUE ARGUMENT
+
+# NOTE PENTING PENULISAN VARIADIC FUNCTION:
+	- PENULISAN VARIADIC FUNCTION TIDAK BOLEH DIAWAL DAN DITENGAH JIKA ANDA MAU MEMBUAT BANYAK PARAMETER.
+	- JIKA MEMBUAT 2 ATAU LEBIH PARAMETER MAKA YANG PARAMETER VARIADIC DITEMPATKAN SEBELAH KIRI
+	- JIKA HANYA MEMBUAT 2 PARAMATER SAJA VARIADIC FUNCTION TIDAK BOLEH DITEMPATKAN DI SEBELAH KANAN
+	- VARIADIC FUNCTION HANYA BOLEH DITEMPATKAN PALING UJUNG SEBELAH KANAN
+
+# CONTOH SYNTAX YANG TIDAK BOLEH:
+	1. membuat paramter lebih dari 2
+		A. func sum (param1 int, nums ...int, param2 int) {
+
+		}
+		B. func sum (nums ...int, param1 int, param2 int) {
+
+		}
+	2. func sum (nums ...int, param1 int) {
+
+		}
+
+# SLICE SEBAGAI PARAMETER
+	- PADA CONTOH SEBELUMNYA KITA MENULISAKAN ANGKA-ANGKA LANGSUNG SAAT MEMENGGIL
+	FUNCTION
+	- SEBENARNYA DEFAULTNYA KITA BISA MEMASUKKAN SLICE SEBAGAI ARGUMENT PADA VARIADIC FUNCTION
+	- KARENA Varargs PADA VARIADIC FUNCTION AKAN DIUBAH MENJADI "SLICE", OLEH KARNA ITU
+	KITA BISA MELAKUKAN PARULANGAN PADA Varrags UNTUK MELAKUKAN PENJUMLAHAN.
+
+*/
+
+package main
+
+import "fmt"
+
+// ...(titik tiga) ini digunakan untuk bisa memasukkan banyak nilai dari argument
+func sum(nums ...int) int {
+
+	// v-total ini digunakan untuk menampung nilai parameter nums diawali dengan 0
+	total := 0
+
+	// melakukan perulangan paramter nums
+	for _, num := range nums { // range nums ini belum tau cara bacanya
+		total += num // nilai dari total ditambah dengan num. jadi awalnya 1+1=2+1=3+3=6+4=10+5=15
+	}
+	return total // lalu kembalikan nilai dari total
+
+}
+
+func main() {
+	// contohnya ini kita memasukkan banyak nilai dengan satu argument dipisahkan dengan koma
+	sumAll := sum(1, 2, 3, 4, 5) // ini nilai paramaternya sudah masuk ke perulangan num
+	fmt.Println(sumAll)          // 15
+}
